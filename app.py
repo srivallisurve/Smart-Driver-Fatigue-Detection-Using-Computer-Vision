ccdac775fbb1a6903445074d1665107042a642ad
srivallisurve <survesri163@gmail.com>
2026-03-14 14:19:22 +0530

    fix CORS

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET","POST","OPTIONS"],
     allow_headers=["*"],
 )
 
